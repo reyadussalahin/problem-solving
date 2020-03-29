56dcd2e2ef83795380a4cd9ed8561e536b091357
reyad <reyadussalahin@gmail.com>
2020-03-29 19:59:16 +0600

    Solution for timus 1000(A+B Problem) added

---
+++ ad_hoc/beginner/timus/F1000/Solution.py
@@ -0,0 +1,13 @@
+# problem name: A+B Problem
+# problem link: https://acm.timus.ru/problem.aspx?space=1&num=1000
+# contest link: (?)
+# time: (?)
+# author: reyad
+
+# other tags: (?)
+
+
+# difficulty level: beginner
+
+a, b = map(int, input().split())
+print(a + b)
