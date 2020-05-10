bb644f719f27635713db571a14305fb2bade6906
reyad <reyadussalahin@gmail.com>
2020-05-10 15:23:46 +0600

    Solution for cf 1345A(Puzzle Pieces) added

---
+++ math/basic/codeforces/F1345A/Solution.cpp
@@ -0,0 +1,35 @@
+// problem_name: Puzzle Pieces
+// problem_link: https://codeforces.com/contest/1345/problem/A
+// contest_link: https://codeforces.com/contest/1345
+// time: (?)
+// author: reyad
+
+// other_tags: game
+// difficulty_level: easy
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+	int tc;
+
+	scanf("%d", &tc);
+	for(int cc=0; cc<tc; cc++) {
+		int n, m;
+		scanf("%d %d", &n, &m);
+
+		if(n == 1 || m == 1) {
+			printf("YES\n");
+		} else {
+			if(n <= 2 && m <= 2) {
+				printf("YES\n");
+			} else {
+				printf("NO\n");
+			}
+		}
+	}
+	return 0;
+}
