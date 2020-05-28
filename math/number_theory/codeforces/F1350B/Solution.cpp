87c69dae77bff1f95509487c190be8bb2da95b76
reyad <reyadussalahin@gmail.com>
2020-05-28 22:04:07 +0600

    Solution for cf 1350B(Orac and Models) added

---
+++ math/number_theory/codeforces/F1350B/Solution.cpp
@@ -0,0 +1,50 @@
+// problem name: Orac and Models
+// problem link: https://codeforces.com/contest/1350/problem/B
+// contest link: https://codeforces.com/contest/1350
+// time: (?)
+// author: reyad
+
+// other_tags: dp
+// difficulty_level: easy
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 100000
+
+vector<int> dv[N+10];
+int b[N+10], r[N+10];
+
+int main() {
+	for(int i=1; i<=N; i++) {
+		for(int j=i+i; j<=N; j+=i) {
+			dv[j].push_back(i);
+		}
+	}
+
+	int tc;
+	scanf("%d", &tc);
+	for(int cc=0; cc<tc; cc++) {
+		int n;
+		scanf("%d", &n);
+		for(int i=1; i<=n; i++) {
+			scanf("%d", &b[i]);
+			r[i] = 1;
+		}
+		for(int i=n; i>=1; i--) {
+			for(int d: dv[i]) {
+				if(b[i] > b[d]) {
+					r[d] = max(r[d], r[i] + 1);
+				}
+			}
+		}
+		int mx = 0;
+		for(int i=1; i<=n; i++) {
+			mx = max(mx, r[i]);
+		}
+		printf("%d\n", mx);
+	}
+	return 0;
+}
