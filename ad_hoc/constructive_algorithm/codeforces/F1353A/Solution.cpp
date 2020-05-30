25fe49c8195dc586079980b7164e1a04890b1c38
reyad <reyadussalahin@gmail.com>
2020-05-30 22:18:10 +0600

    Solution for cf 1353A(Most Unstable Array) added

---
+++ ad_hoc/constructive_algorithm/codeforces/F1353A/Solution.cpp
@@ -0,0 +1,32 @@
+// problem name: Most Unstable Array
+// problem link: https://codeforces.com/contest/1353/problem/A
+// contest link: https://codeforces.com/contest/1353
+// time: (?)
+// author: reyad
+
+// other_tags: greedy, math
+// difficulty_level: easy
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int tc;
+	scanf("%d", &tc);
+	for(int cc=0; cc<tc; cc++) {
+		int n, m;
+		scanf("%d %d", &n, &m);
+		long long ans = 0;
+		if(n == 1) {
+			ans = 0LL;
+		} else if(n == 2) {
+			ans = m;
+		} else {
+			ans = 2LL * m;
+		}
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
