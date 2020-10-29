975aefdfbe181459dd6a2996dbaac3f284d6d5ef
reyadussalahin <reyadussalahin@gmail.com>
2020-10-30 01:54:46 +0600

    leetcode-1282: fixed some small errors

--- greedy/basic/leetcode/F1282/Solution.py
+++ greedy/basic/leetcode/F1282/Solution.py
@@ -1,14 +1,16 @@
-# problem name: 1282. Group the People Given the Group Size They Belong To
+# problem name: Group the People Given the Group Size They Belong To
 # problem link: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
 # contest link: (?)
-# time: 196ms
+# time: (?)
 # author: ratul14
 
 
 # other_tags: (?)
 # difficulty_level: medium
 
-#submission_link: https://leetcode.com/submissions/detail/405650239/
+
+# submission_link: https://leetcode.com/submissions/detail/405650239/
+
 
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
