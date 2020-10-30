e3e369e8423ce94c8ce470a1c38d27320f3fdb4f
P7 <ratulroy20@gmail.com>
2020-10-30 08:55:17 +0600

    Solution for leetcode1282 (version2)

---
+++ greedy/basic/leetcode/F1282/Solution001.py
@@ -0,0 +1,28 @@
+# problem name: Group the People Given the Group Size They Belong To
+# problem link: https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/
+# contest link: (?)
+# time: (?)
+# author: ratul14
+
+
+# other_tags: (?)
+# difficulty_level: medium
+
+#Submission_details: https://leetcode.com/submissions/detail/405650736/
+
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groups = []
+        tracker = {}
+        
+        for i in range(len(groupSizes)):
+            size = groupSizes[i]
+            
+            if size not in tracker or tracker[size][1] == size:
+                tracker[size] = [len(groups), 0]
+                groups.append([])
+            
+            groups[tracker[size][0]].append(i) 
+            tracker[size][1] += 1
+            
+        return groups
