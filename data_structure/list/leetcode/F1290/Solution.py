47647067e38441584213e31e818fa429e475ae35
reyadussalahin <reyadussalahin@gmail.com>
2020-10-31 22:45:15 +0600

    leetcode-1290: added review changes
    - leetcode-1290:
      - changes:
        - removed unnecessary submission details
        - removed redundant leetcode template

--- data_structure/list/leetcode/F1290/Solution.py
+++ data_structure/list/leetcode/F1290/Solution.py
@@ -9,15 +9,6 @@
 # difficulty_level: easy
 
 
-
-
-#submission_details: https://leetcode.com/submissions/detail/363880101/
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         binary = 0;
