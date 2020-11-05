a835faae45e46992d396cfcc69fcb08b323d9363
reyadussalahin <reyadussalahin@gmail.com>
2020-11-05 23:31:55 +0600

    loj-[1105, 1100]: solution added
    loj-1105:
      - file: math/combinatorics/loj/F1105/Solution.cpp
        - new solution added
    
    loj-1100:
      - file: search/binary_search/loj/F1100/Solution.cpp
        - new solution added

---
+++ math/combinatorics/loj/F1105/Solution.cpp
@@ -0,0 +1,58 @@
+// problem name: Fi Binary Number 
+// problem link: http://lightoj.com/volume_showproblem.php?problem=1105
+// contest link: (?)
+// time: (?)
+// author: reyad
+
+
+// other_tags: fibonacci
+// difficulty_level: easy
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define N 1000010
+#define M 1000000000
+ 
+int stck[128];
+char str[128];
+ 
+int main() {
+    int tc;
+    scanf("%d", &tc);
+    for(int cc=0; cc<tc; cc++) {
+        int n;
+        scanf("%d", &n);
+        int top = 0;
+        while(n > 0) {
+            int a = 1, b = 2;
+            int i = 1;
+            while(b <= n) {
+                int c = a + b;
+                a = b;
+                b = c;
+                i++;
+            }
+            stck[top++] = i - 1;
+            n = n - a;
+        }
+        int l = stck[0];
+        for(int i=0; top > 0; i++) {
+            if(i == stck[top-1]) {
+                str[i] = '1';
+                top--;
+            } else {
+                str[i] = '0';
+            }
+        }
+        printf("Case %d: ", cc+1);
+        for(int i=l; i>=0; i--) {
+            printf("%c", str[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
