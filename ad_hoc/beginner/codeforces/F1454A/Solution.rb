9e4a2632000a2443e7faec46649c7bfa855051a7
reyadussalahin <reyadussalahin@gmail.com>
2020-12-08 23:51:56 +0600

    cf-1454A: solution added
    cf-1454A:
      - file: "ad_hoc/beginner/codeforces/F1454A/Solution.rb"
        - new solution added

---
+++ ad_hoc/beginner/codeforces/F1454A/Solution.rb
@@ -0,0 +1,19 @@
+# problem name: Special Permutation
+# problem link: https://codeforces.com/contest/1454/problem/A
+# contest link: https://codeforces.com/contest/1454
+# time: (?)
+# author: reyad
+
+
+# other_tags: constructive algorithms, probabilities
+# difficulty_level: easy
+
+
+tc = gets.chomp.to_i
+(1..tc).each do |cc|
+  n = gets.chomp.to_i
+  (2..n).each do |i|
+    print i, " "
+  end
+  puts 1
+end
