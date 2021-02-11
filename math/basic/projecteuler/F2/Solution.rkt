0272affe64daf6e561e026489d9af6b736bc795e
reyadussalahin <reyadussalahin@gmail.com>
2021-02-12 03:46:10 +0600

    Solution added for projecteuler-02
    projecteuler-02:
      - file: math/basic/projecteuler/F2/Solution.rkt
        - new solution added
        - language used: Racket

---
+++ math/basic/projecteuler/F2/Solution.rkt
@@ -0,0 +1,26 @@
+; problem name: Even Fibonacci numbers
+; problem link: https://projecteuler.net/problem=2
+; contest link: (?)
+; time: (?)
+; author: reyad
+
+
+; other_tags: (?)
+; difficulty_level: beginner
+
+
+#lang racket
+
+; answer: 4613732
+
+(define fib-sum
+  (lambda (a b)
+    (cond
+      ((> b 4000000)
+       0)
+      ((= (remainder b 2) 0)
+       (+ b (fib-sum b (+ a b))))
+      (#t
+       (fib-sum b (+ a b))))))
+
+(fib-sum 1 2)
