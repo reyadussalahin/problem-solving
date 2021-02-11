9f48b001632ab307375a7e387b4c7337b3703074
reyadussalahin <reyadussalahin@gmail.com>
2021-02-12 03:21:21 +0600

    Solution added for projecteuler-01
    projecteuler-01:
      - file: math/basic/projecteuler/F1/Solution.rkt
        - new solution added
        - language used: Racket

---
+++ math/basic/projecteuler/F1/Solution.rkt
@@ -0,0 +1,29 @@
+; problem name: Multiples of 3 and 5
+; problem link: https://projecteuler.net/problem=1
+; contest link: (?)
+; time: (?)
+; author: reyad
+
+
+; other_tags: (?)
+; difficulty_level: beginner
+
+
+#lang racket
+
+; answer: 233168
+
+
+(define mul-sum
+  (lambda (n)
+    (cond
+      ((= n 0)
+       0)
+      ((or (= (remainder n 3) 0)
+           (= (remainder n 5) 0))
+       (+ n (mul-sum (- n 1))))
+      (#t
+       (mul-sum (- n 1))))))
+
+(mul-sum 999)
+
