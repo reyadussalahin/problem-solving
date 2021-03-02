20d380486eefe3db17450e184885c807582153ad
reyadussalahin <reyadussalahin@gmail.com>
2021-03-03 00:35:48 +0600

    Solution for projecteuler-16 added
    projecteuler-16:
      - file: "math/big_integer/projecteuler/F16/Solution.rkt"
        - new solution added
        - language used: `Racket`

---
+++ math/big_integer/projecteuler/F16/Solution.rkt
@@ -0,0 +1,34 @@
+; problem name: Power digit sum
+; problem link: https://projecteuler.net/problem=16
+; contest link: (?)
+; time: (?)
+; author: reyad
+
+
+; other_tags: loop, recursion
+; difficulty_level: beginner
+
+
+#lang racket
+
+; answer: 1366
+
+(define char-digit-to-int-digit
+  (lambda (ch)
+    (- (char->integer ch)
+       (char->integer #\0))))
+
+(define reduce
+  (lambda (fn l init)
+    (cond
+      [(null? l)
+       init]
+      [#t
+       (fn (car l)
+           (reduce fn (cdr l) init))])))
+
+(reduce (lambda (x y)
+          (+ (char-digit-to-int-digit x)
+             y))
+        (string->list (number->string (expt 2 1000)))
+        0)
