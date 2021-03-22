b06c23ca88037ea9704cc3977cd97c6e80f9dbc2
reyadussalahin <reyadussalahin@gmail.com>
2021-03-22 23:38:07 +0600

    Solution for leetcode-1800 added
    leetcode-1800:
      - file: "ad_hoc/beginner/leetcode/F1800/Solution.py"
      - new solution added

---
+++ ad_hoc/beginner/leetcode/F1800/Solution.py
@@ -0,0 +1,22 @@
+# problem name: Maximum Ascending Subarray Sum
+# problem link: https://leetcode.com/contest/weekly-contest-233/problems/maximum-ascending-subarray-sum/
+# contest link: https://leetcode.com/contest/weekly-contest-233/
+# time: (?)
+# author: reyad
+
+
+# other_tags: two pointers
+# difficulty_level: easy
+
+
+
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        temp_sum = nums[0]
+        max_sum = temp_sum
+        for i in range(1, len(nums)):
+            if nums[i-1] >= nums[i]:
+                temp_sum = 0
+            temp_sum += nums[i]
+            max_sum = max(max_sum, temp_sum)
+        return max_sum
