45dec89de8582254ea06fef1fe586564de7f926d
reyadussalahin <reyadussalahin@gmail.com>
2021-03-27 23:28:48 +0600

    solution for leetcode-1798 added
    leetcode-1798:
      - file: "greedy/basic/leetcode/F1798/Solution.py"
      - new solution added

---
+++ greedy/basic/leetcode/F1798/Solution.py
@@ -0,0 +1,19 @@
+# problem name: Maximum Number of Consecutive Values You Can Make
+# problem link: https://leetcode.com/contest/biweekly-contest-48/problems/maximum-number-of-consecutive-values-you-can-make/
+# contest link: https://leetcode.com/contest/biweekly-contest-48/
+# time: (?)
+# author: reyad
+
+
+# other_tags: (?)
+# difficulty_level: beginner
+
+
+class Solution:
+    def getMaximumConsecutive(self, coins: List[int]) -> int:
+        ans = 1
+        for v in sorted(coins):
+            if v > ans:
+                break
+            ans += v
+        return ans
