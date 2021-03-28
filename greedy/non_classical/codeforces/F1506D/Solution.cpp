8684c563a5a2f47eb60844ed53ec726c859fe915
reyadussalahin <reyadussalahin@gmail.com>
2021-03-28 15:27:37 +0600

    solution for cf-1506D added
    cf-1506D:
      - file: "greedy/non_classical/codeforces/F1506D/Solution.cpp"
      - new solution added

---
+++ greedy/non_classical/codeforces/F1506D/Solution.cpp
@@ -0,0 +1,58 @@
+// problem name: Epic Transformation
+// problem link: https://codeforces.com/contest/1506/problem/D
+// contest link: https://codeforces.com/contest/1506
+// time: (?)
+// author: reyad
+
+
+// other_tags: constructive algorithm, data structure
+// difficulty_level: beginner
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tc;
+    scanf("%d", &tc);
+    for(int cc=0; cc<tc; cc++) {
+        map<int, int> mp;
+        priority_queue<int> pq;
+        int n;
+        scanf("%d", &n);
+        for(int i=0; i<n; i++) {
+            int a;
+            scanf("%d", &a);
+            mp[a]++;
+        }
+        for(auto it: mp) {
+            pq.push(it.second);
+        }
+        while(!pq.empty()) {
+            int x = pq.top();
+            pq.pop();
+            if(pq.empty()) {
+                pq.push(x);
+                break;
+            }
+            int y = pq.top();
+            pq.pop();
+            // printf("x, y: %d\n", x, y);
+            --x; --y;
+            if(x) {
+                pq.push(x);
+            }
+            if(y) {
+                pq.push(y);
+            }
+        }
+        int ans = 0;
+        while(!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
