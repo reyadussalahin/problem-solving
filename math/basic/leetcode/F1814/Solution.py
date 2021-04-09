7ea474af2e6750801212b91a95a7cd67c6e177fb
reyadussalahin <reyadussalahin@gmail.com>
2021-04-09 23:13:13 +0600

    solution for leetcode-1814 added
    leetcode-1814:
      - file: "math/basic/leetcode/F1814/Solution.py"
      - new solution added

---
+++ math/basic/leetcode/F1814/Solution.py
@@ -0,0 +1,24 @@
+# problem name: Count Nice Pairs in an Array
+# problem link: https://leetcode.com/contest/biweekly-contest-49/problems/count-nice-pairs-in-an-array/
+# contest link: https://leetcode.com/contest/biweekly-contest-49/
+# time: (?)
+# author: reyad
+
+
+# other_tags: (?)
+# difficulty_level: beginner
+
+
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        rnums = [int(str(n)[::-1]) for n in nums]
+        cnt = {}
+        for n, rn in zip(nums, rnums):
+            d = n - rn
+            if d not in cnt:
+                cnt[d] = 0
+            cnt[d] += 1
+        ans = 0
+        for _, c in cnt.items():
+            ans += (c * (c-1)) // 2
+        return ans % 1000000007
