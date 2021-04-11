fa632cad0aec4aabfc7ced2d70ef45babcfd6ae7
reyadussalahin <reyadussalahin@gmail.com>
2021-04-11 23:56:29 +0600

    solution for cf-1475C added
    cf-1475C:
      - file: "math/combinatorics/codeforces/F1475C/Solution.cpp"
      - new solution added

---
+++ math/combinatorics/codeforces/F1475C/Solution.cpp
@@ -0,0 +1,46 @@
+// problem name: Ball in Berland
+// problem link: https://codeforces.com/contest/1475/problem/C
+// contest link: https://codeforces.com/contest/1475/
+// time: (?)
+// author: reyad
+
+
+// other_tags: graph, inclusion exclusion principle
+// difficulty_level: beginner
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 200020
+
+int aa[N], bb[N];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    for(int cc=0; cc<tc; cc++) {
+        int a, b, k;
+        map<int, int> boy;
+        map<int, int> girl;
+        cin >> a >> b >> k;
+        for(int i=0; i<k; i++) {
+            cin >> aa[i];
+            boy[aa[i]]++;
+        }
+        for(int i=0; i<k; i++) {
+            cin >> bb[i];
+            girl[bb[i]]++;
+        }
+        long long ans = 0;
+        for(int i=0; i<k; i++) {
+            long long t = k - (boy[aa[i]] + girl[bb[i]] - 1);
+            ans += t;
+        }
+        cout << (ans / 2) << "\n";
+    }
+    return 0;
+}
