da4f66ba8d1eecdc360232eb3ae409df6619593d
reyadussalahin <reyadussalahin@gmail.com>
2021-04-15 23:06:56 +0600

    solution for cf-1512A added
    cf-1512A:
      - file: "ad_hoc/implementation/codeforces/F1512A/Solution.cpp"
      - new solution added

---
+++ ad_hoc/implementation/codeforces/F1512A/Solution.cpp
@@ -0,0 +1,38 @@
+// problem name: Spy Detected!
+// problem link: https://codeforces.com/contest/1512/problem/A
+// contest link: https://codeforces.com/contest/1512
+// time: (?)
+// author: reyad
+
+
+// other_tags: brute force
+// difficulty_level: beginner
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    for(int cc=0; cc<tc; cc++) {
+        int n;
+        cin >> n;
+        map<int, vector<int>> mp;
+        for(int i=0; i<n; i++) {
+            int a;
+            cin >> a;
+            mp[a].push_back(i);
+        }
+        for(auto it: mp) {
+            if((int)it.second.size() == 1) {
+                cout << it.second[0] + 1 << "\n";
+                break;
+            }
+        }
+    }
+    return 0;
+}
